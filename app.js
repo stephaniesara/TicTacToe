76e8bf6b61e044fccf21c44f641f823ae302a4e4
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-02-12 12:06:11 -0800

    Create board model and listeners on board

--- app.js
+++ app.js
@@ -3,3 +3,34 @@
 // 	document.getElementById("")
 // })
 
+// MODEL
+
+var initBoard = (size) => {
+	var board = [];
+	for (var i = 0; i < size; i++) {
+		board[i] = new Array(size).fill(null);
+	}
+	return board;
+}
+
+const size = 3;
+var board = initBoard(size); // global variable for board
+
+
+// CONTROLLER
+
+document.addEventListener("DOMContentLoaded", (event) => {
+	// console.log("DOM loaded!");
+	document.getElementById("A").addEventListener("click", function() {
+		callback("A");
+	});
+	document.getElementById("B").addEventListener("click", function() {
+		callback("B");
+	});
+});
+
+// VIEW
+
+var callback = (str) => {
+	console.log(str);
+}
