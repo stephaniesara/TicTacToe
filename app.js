107d7123342ca6d1da2c819d1fba8286d1fe4a40
Stephanie Sara Chong <stephaniesarachong@Stephanies-MacBook-Air-2.local>
2018-02-12 16:55:41 -0800

    Complete implementation for bare minimum reqs

--- app.js
+++ app.js
@@ -14,10 +14,78 @@ var initBoard = (size) => {
 	return board;
 }
 
+var toggleSquareModel = (row, col) => {
+	board[row][col] = player;
+};
 
+var hasTie = () => {
+	var isFull = true;
+	board.forEach((row) => {
+		row.forEach((square) => {
+			if (square === null) {
+				isFull = false;
+			}
+		});
+	});
+	return isFull;
+};
+
+var isRowFull = (row) => {
+	return board[row].every((square) => {
+		return square === player;
+	});
+};
+
+var isColFull = (col) => {
+	for (var i = 0; i < size; i++) {
+		if (board[i][col] !== player) {
+			return false;
+		}
+	}
+	return true;
+};
+
+var isMajorDiagFull = (row, col) => {
+	if (row + col !== 2) {
+		return false;
+	}
+	if (board[0][2] !== player || board[1][1] !== player || board[2][0] !== player) {
+		return false;
+	}
+	return true;
+};
+
+var isMinorDiagFull = (row, col) => {
+	if (row !== col) {
+		return false;
+	}
+	if (board[0][0] !== player || board[1][1] !== player || board[2][2] !== player) {
+		return false;
+	}
+	return true;
+};
+
+var hasWinner = (row, col) => {
+	return (isRowFull(row) || isColFull(col) || isMajorDiagFull(row, col) || isMinorDiagFull(row, col) );
+};
+
+var isGameOver = (row, col) => {
+	if (hasWinner(row, col)) {
+		updateMessageView('Game over! The winner is ' + symbols[player] + '!');
+		return true;
+	} else if (hasTie()) {
+		updateMessageView('Game over! Both players have tied.');
+		return true;
+	} else {
+		updateMessageView('Next up: ' + symbols[!player]);
+		return false; // continue playing
+	}
+};
 
 // CONTROLLER
 
+
+// should refactor this to include listeners directly in html dom elements
 var setupListeners = () => {
 	var td;
 	for (var row = 0; row < size; row++) {
@@ -26,27 +94,70 @@ var setupListeners = () => {
 			td = document.getElementById(id);
 			(function (td) {
 				td.addEventListener('click', function() {
-					handleClick(Number(td.id.charAt(0)), Number(td.id.charAt(1)));
+					var row = Number(td.id.charAt(0));
+					var col = Number(td.id.charAt(1));
+					if (playing && board[row][col] === null) {
+						handleClick(td, row, col);
+					}
 				})
 			})(td)
 		}
 	}
 };
 
-var handleClick = (row, col) => {
-	console.log(row, col);
-	console.log(typeof(row))
+var handleClick = (elem, row, col) => {
+	toggleSquareView(elem);
+	toggleSquareModel(row, col);
+	if (isGameOver(row, col)) {
+		console.log('game over!');
+		playing = false; // end game
+	} else {
+		console.log('keep playing!');
+		player = !player; // switch turns
+	}
+};
+
+// VIEW
+
+var updateMessageView = (message) => {
+	document.getElementById('message').innerHTML = message;
 };
 
 
+var toggleSquareView = (elem) => {
+	var tile = symbols[player];
+	elem.innerHTML = tile;
+};
+
+var clearBoardView = () => {
+	squares = Array.prototype.slice.call(document.getElementsByClassName('square'));
+	squares.forEach((square) => {
+		square.innerHTML = '';
+	});
+};
+
 // GLOBALS
 
+var player; 
+var playing; // true while game is not over
+var symbols = {
+	true: 'X',
+	false: 'O'
+}
 const size = 3;
 var board; // global variable for board
 
+var newGame = () => {
+	updateMessageView('Let\'s tic tac toe! First player is X');
+	clearBoardView();
+	init();
+};
+
 var init = () => {
 	board = initBoard(size);
-	setupListeners();
+	player = true; // for player X
+	playing = true;
 }
 
+setupListeners();
 init();
